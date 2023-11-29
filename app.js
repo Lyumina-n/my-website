2274af2ef7f2c4b46def937ae7690fdc8fe5ce93
Lyumina-n <nedokosl@mail.com>
2023-11-29 14:51:34 +0200

    nav bar links navigation to sections js properties added

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-var swiper = new Swiper(`.mySwiper`, {
+window.addEventListener('scroll', function(){
+  var scrollPosition=window.scrollY;
+  var sections=document.querySelectorAll('section');
+
+  sections.forEach(function(section){
+    var top=section.offsetTop;
+    var height=section.offsetHeight;
+
+    if(scrollPosition>=top&&scrollPosition<top+height){
+      var id=section.getAttribute('id');
+      document.querySelector('nav a[href="#'+id+'"]').classList.add('active');
+    }else{
+      var id=section.getAttribute('id');
+      document.querySelector('nav a[href="#'+id+'"]').classList.remove('active');
+    }
+  });
+});
+
+var swiper = new Swiper(".mySwiper", {
       slidesPerView: 2,
       centeredSlides:false,
       slidesPerGroupSkip:1,
